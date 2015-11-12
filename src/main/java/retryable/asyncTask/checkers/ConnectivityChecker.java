1159b1ac7478ca82a5acf140bebfce4b98afe69a
Alabê Duarte <alabeduarte@gmail.com>
2015-11-12 10:53:14 -0300

    Introduce ConnectivityChecker

---
+++ src/main/java/retryable/asyncTask/checkers/ConnectivityChecker.java
@@ -0,0 +1,26 @@
+package retryable.asyncTask.checkers;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class ConnectivityChecker {
+  private final Context context;
+
+  public ConnectivityChecker(Context context) {
+    this.context = context;
+  }
+
+  public boolean isConnected() {
+    return getActiveNetworkInfo() != null &&
+        getActiveNetworkInfo().isConnectedOrConnecting();
+  }
+
+  private NetworkInfo getActiveNetworkInfo() {
+    return getConnectivityManager().getActiveNetworkInfo();
+  }
+
+  private ConnectivityManager getConnectivityManager() {
+    return (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+  }
+}
---
+++ src/test/java/retryable/asyncTask/checkers/ConnectivityCheckerTest.java
@@ -0,0 +1,85 @@
+package retryable.asyncTask.checkers;
+
+import android.app.Activity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowNetworkInfo;
+
+import retryable.asyncTask.BuildConfig;
+
+import static android.content.Context.CONNECTIVITY_SERVICE;
+import static android.net.ConnectivityManager.TYPE_WIFI;
+import static android.net.NetworkInfo.DetailedState.CONNECTED;
+import static android.net.NetworkInfo.DetailedState.DISCONNECTED;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.robolectric.Robolectric.buildActivity;
+import static org.robolectric.RuntimeEnvironment.application;
+import static org.robolectric.Shadows.shadowOf;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class ConnectivityCheckerTest {
+
+  ConnectivityChecker connectivityChecker;
+  Activity activity;
+
+  @Before
+  public void setUp() {
+    activity = buildActivity(Activity.class).create().get();
+
+    connectivityChecker = new ConnectivityChecker(activity);
+  }
+
+  @Test
+  public void itReturnsFalseWhenThereIsNoActivityInfoAvailable() {
+    shadowOf(getConnectivityManager()).setActiveNetworkInfo(null);
+
+    assertThat(connectivityChecker.isConnected(), is(false));
+  }
+
+  @Test
+  public void itReturnsTrueWhenThereIsConnectivity() {
+    connectToWifiNetwork();
+
+    assertThat(connectivityChecker.isConnected(), is(true));
+  }
+
+  @Test
+  public void itReturnsFalseWhenThereIsNoConnectivity() {
+    disconnectToWifiNetwork();
+
+    assertThat(connectivityChecker.isConnected(), is(false));
+  }
+
+  private void connectToWifiNetwork() {
+    setWitiNetworkConnectivity(CONNECTED, true);
+  }
+
+  private void disconnectToWifiNetwork() {
+    setWitiNetworkConnectivity(DISCONNECTED, false);
+  }
+
+  private void setWitiNetworkConnectivity(NetworkInfo.DetailedState status, boolean isConnected) {
+    ConnectivityManager connectivityManager = getConnectivityManager();
+    int subType = 0;
+    boolean isAvailable = true;
+
+    NetworkInfo networkInfo = ShadowNetworkInfo.newInstance(
+        status, TYPE_WIFI, subType, isAvailable, isConnected
+    );
+
+    shadowOf(connectivityManager).setActiveNetworkInfo(networkInfo);
+  }
+
+  private ConnectivityManager getConnectivityManager() {
+    return (ConnectivityManager) application.getSystemService(CONNECTIVITY_SERVICE);
+  }
+
+}
